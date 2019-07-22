83b1ad4a31857b59bc172d8fc0921e7ea99fb968
Lukasz Drozd <principium17waw@gmail.com>
2019-07-22 21:29:56 +0200

    6.6 Zadanie

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/Book.java
@@ -0,0 +1,52 @@
+package com.kodilla.testing.library;
+
+public class Book {
+    String title;
+    String author;
+    Integer publicationYear;
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publicationYear=" + publicationYear +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear.equals(book.publicationYear) &&
+                title.equals(book.title) &&
+                author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + author.hashCode();
+        result = 31 * result + publicationYear.hashCode();
+        return result;
+    }
+}
