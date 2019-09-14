c0087eaa6e51b9b790ebe2efacb19c39de3a8e2b
LukaszMDrozd <principium17waw@gmail.com>
2019-09-14 15:48:44 +0200

    Zadanie 13.4 update 14.09.2019

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodOrder/FoodOrderRequest.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/foodOrder/FoodOrderRequest.java
@@ -3,21 +3,21 @@ package com.kodilla.good.patterns.foodOrder;
 public class FoodOrderRequest {
 
     private FoodProducer foodProducer;
-    private String FoodQuantity;
-    private String FoodType;
+    private String foodQuantity;
+    private String foodType;
 
     public FoodOrderRequest(final FoodProducer foodProducer, final String foodQuantity, final String foodType) {
         this.foodProducer = foodProducer;
-        FoodQuantity = foodQuantity;
-        FoodType = foodType;
+        this.foodQuantity = foodQuantity;
+        this.foodType = foodType;
     }
 
     public String getFoodQuantity() {
-        return FoodQuantity;
+        return foodQuantity;
     }
 
     public String getFoodType() {
-        return FoodType;
+        return foodType;
     }
 
     public FoodProducer getFoodProducer() {
